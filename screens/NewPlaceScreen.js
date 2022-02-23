67df7fe776cde51ef994a40ff0344172bb1cecfa
Marcelo Junior <marcelohjw@gmail.com>
2022-02-23 18:37:42 -0300

    Standalone configuration done

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PlacesListScreen = props => {
+    return (
+        <View>
+            <Text>New place Screen!</Text>
+        </View>
+    );
+};
+
+export default PlacesListScreen;
---
+++ screens/PlaceDetailScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PlacesListScreen = props => {
+    return (
+        <View>
+            <Text>Places detail Screen!</Text>
+        </View>
+    );
+};
+
+export default PlacesListScreen;
