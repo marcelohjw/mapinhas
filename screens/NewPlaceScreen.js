57490ff71ed0561df29f1dd22ca56c9843b372d1
Marcelo Junior <marcelohjw@gmail.com>
2022-02-25 10:08:25 -0300

    Getting Started with the form

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView, Alert } from 'react-native';
+
+import Colors from '../constants/Colors';
 
 const NewPlaceScreen = props => {
+    const [titleValue, setTitleValue] = useState('');
+
+    const titleChangeHandler = text => {
+        // Validação aqui
+        setTitleValue(text);
+    };
+
+    const savePlaceHandler = () => {
+        Alert.alert(titleValue);
+    };
+
     return (
-        <View style={styles.centered}>
-            <Text>Tela de adicionar lugar!</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Nome</Text>
+                <TextInput 
+                    style={styles.textInput} 
+                    onChangeText={titleChangeHandler}
+                    value={titleValue}
+                />
+                <Button 
+                    title='Salvar' 
+                    color={Colors.primary} 
+                    onPress={savePlaceHandler}
+                />
+            </View>
+        </ScrollView>
     );
 };
 
@@ -14,10 +39,19 @@ NewPlaceScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
-    centered: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    form: {
+        margin: 30
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2
     }
 });
 
